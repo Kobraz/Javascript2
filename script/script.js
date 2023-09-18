79f52ec6985faa83ea20de5be772b3c17e3daa8e
Kobraz <roar.f.hanssen@gmail.com>
2023-09-18 13:24:21 +0200

    login-form with email and password check

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ let signupBtn = document.getElementById("signupBtn");
 let signinBtn = document.getElementById("signinBtn");
 let nameField = document.getElementById("nameField");
 let title = document.getElementById("title");
+let message = document.getElementById("message");
 
 signinBtn.onclick = function () {
   nameField.style.maxHeight = "0";
@@ -16,3 +17,28 @@ signupBtn.onclick = function () {
   signinBtn.classList.add("disabled");
   signupBtn.classList.remove("disabled");
 };
+
+//---testing---
+
+const loginForm = document.getElementById("loginForm");
+const users = [
+  { email: "u1@gmail.com", password: "pass1" },
+  { email: "u2@gmail.com", password: "pass2" },
+  { email: "u3@gmail.com", password: "pass3" },
+];
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const user = users.find(
+    (user) => user.email === email && user.password === password
+  );
+  if (user) {
+    alert("Login successful!");
+    message.innerHTML = "Login successful!";
+  } else {
+    alert("Invalid username or password.");
+    message.innerHTML = "Invalid username or password.";
+  }
+});
