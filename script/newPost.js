18db9089826b367f5ddc834872ef41a3952bc36a
Kobraz <roar.f.hanssen@gmail.com>
2023-09-22 22:55:43 +0200

    added file-reader to my javascript

--- script/newPost.js
+++ script/newPost.js
@@ -8,9 +8,10 @@
 // div.innerHTML = text;
 // document.body.appendChild(div);
 
-var text = document.getElementById("textField").value;
-var blob = new Blob([text], { type: "text/plain" });
-var a = document.createElement("a");
-a.href = URL.createObjectURL(blob);
-a.download = "myFile.txt";
-a.click();
+var file = new File("myFile.txt");
+var reader = new FileReader();
+reader.onload = function () {
+  var text = reader.result;
+  console.log(text);
+};
+reader.readAsText(file);
