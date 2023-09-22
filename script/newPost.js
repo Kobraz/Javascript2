99094bbe781bf7440773b51c0cd214e5c92390f4
Kobraz <roar.f.hanssen@gmail.com>
2023-09-22 22:48:35 +0200

    corrected newPost.js code

--- script/newPost.js
+++ script/newPost.js
@@ -1,9 +1,16 @@
+// var text = document.getElementById("textField").value;
+
+// var file = new File("myFile.txt", "w");
+
+// file.write(text);
+
+// var div = document.createElement("div");
+// div.innerHTML = text;
+// document.body.appendChild(div);
+
 var text = document.getElementById("textField").value;
-
-var file = new File("myFile.txt", "w");
-
-file.write(text);
-
-var div = document.createElement("div");
-div.innerHTML = text;
-document.body.appendChild(div);
+var blob = new Blob([text], { type: "text/plain" });
+var a = document.createElement("a");
+a.href = URL.createObjectURL(blob);
+a.download = "myFile.txt";
+a.click();
